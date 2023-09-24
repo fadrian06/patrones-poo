2a2821fb0419a412d05264bd1fe1efb53110e51b
fadrian06 <franyeradriansanchez@gmail.com>
2023-09-23 20:26:45 -0400

    Objetos, clases en Java

---
+++ Fundamentos de la POO/Objetos, clases/src/Aplicacion.java
@@ -0,0 +1,19 @@
+class Aplicacion {
+	public static void main(String[] args) {
+		Gato oscar = new Gato();
+		oscar.nombre = "Óscar";
+		oscar.sexo = "macho";
+		oscar.edad = 3;
+		oscar.peso = 7f;
+		oscar.color = "marrón";
+		oscar.textura = "rayada";
+
+		Gato luna = new Gato();
+		luna.nombre = "Luna";
+		luna.sexo = "Hembra";
+		luna.edad = 2;
+		luna.peso = 5f;
+		luna.color = "gris";
+		luna.textura = "lisa";
+	}
+}
---
+++ Fundamentos de la POO/Objetos, clases/src/Gato.java
@@ -0,0 +1,14 @@
+class Gato {
+	public String nombre;
+	public String sexo;
+	public Integer edad;
+	public Float peso;
+	public String color;
+	public String textura;
+
+	void respirar() {}
+	void comer(Comida comida) {}
+	void correr(Destino destino) {}
+	void dormir(Integer horas) {}
+	void maullar() {}
+}
